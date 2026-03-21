283b73b9bff1b2669973b6e195c03391741bb97b
alexzhal <a.martiushev@innopolis.university>
2026-03-21 03:03:25 +0300

    Add bot scaffold with handlers and test mode

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,7 @@
+from .start import start
+from .help import help
+from .health import health
+from .labs import labs
+from .scores import scores
+
+__all__ = ["start", "help", "health", "labs", "scores"]
